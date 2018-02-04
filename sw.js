f3b0be477ef7bf30f1726e18aa94dfb33d7e1f0a
Tom Nicolosi <tom@teeehn.com>
2018-02-03 19:55:11 -0800

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/tom-nicolosi-blog/app-2ad557eda78799caf139.js","5005d6e56da85c14a3756fa647f86ddf"],["/tom-nicolosi-blog/app-73bd7dfee68c0e8a0711.js","f0ab5592ed9b9cc67c429983bdfd7ae3"],["/tom-nicolosi-blog/app-fe51c988783afee3d5e2.js","af94518e88fa415e767f5bc75130bc8f"],["/tom-nicolosi-blog/commons-c9201d6f19c634d0f23b.js","857447c5fa9a08b9caca4e0d72fc6d35"],["/tom-nicolosi-blog/index.html","95c46be4e70d3c1ca65f18004d947698"],["/tom-nicolosi-blog/offline-plugin-app-shell-fallback/index.html","c18148dec599b17348a08eb41f4fc1d3"]];
+var precacheConfig = [["/tom-nicolosi-blog/app-73bd7dfee68c0e8a0711.js","f0ab5592ed9b9cc67c429983bdfd7ae3"],["/tom-nicolosi-blog/commons-c9201d6f19c634d0f23b.js","857447c5fa9a08b9caca4e0d72fc6d35"],["/tom-nicolosi-blog/index.html","c53676630f55394ec8a31c45c051a989"],["/tom-nicolosi-blog/offline-plugin-app-shell-fallback/index.html","625e244892df6fbe1d093f4573596795"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
